f2996c277737a09d24972b4c0457b56a6adeba82
Narendra <narendrajagandhane@gmail.com>
2022-03-23 17:13:48 -0700

    Day 4 Shopping System Project

--- 2022-03-23/src/com/shopping/ShoppingSystem.java
+++ 2022-03-23/src/com/shopping/ShoppingSystem.java
@@ -6,10 +6,11 @@ public class ShoppingSystem {
 
         Customer c1=new Customer();
         c1.setName("Ajay");
+        c1.setName("Rahul");
         String Name=c1.getName();
         c1.login(Name);
         c1.logout();
-        c1.selectProduct("Rahul");
+        c1.selectProduct("Cloths");
 
         System.out.println("\n ******************Product Class Operation********************");
 
@@ -19,13 +20,14 @@ public class ShoppingSystem {
         p1.changePrice();
         p1.setQuantity_available(300);
         int quantity=p1.getQuantity_available();
-        System.out.println("\n Quantity available"+quantity);
+        System.out.println("\n Quantity available:-"+quantity);
         p1.order();
 
         System.out.println("\n********************** Shopping Cart Operation********************");
 
         ShoppingCart sc=new ShoppingCart();
         sc.add("Towel");
-        sc.remove("Rahi");
+        sc.add("Rice");
+        sc.remove("Towel");
     }
 }
