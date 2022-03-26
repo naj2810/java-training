46cef059cef19e217794a6de931ce63f87a1c6c5
Narendra <narendrajagandhane@gmail.com>
2022-03-25 17:27:31 -0700

    Day1 Practice

--- Day1/Practice1.java
+++ Day1/Practice1.java
@@ -9,8 +9,8 @@ class Practice1
            System.out.println("\n Hello World");
 
 
-       System.out.println("5*3:="+(5*3));
-       System.out.println("Number Of seconds in day :"+60*60*24);
+       System.out.println("\n 5*3:="+(5*3));
+       System.out.println("\n Number Of seconds in day :"+60*60*24);
     }
 
 }
