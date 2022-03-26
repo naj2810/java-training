88b3a598fdc9e7bfa0b617296c19b84f02d41600
Narendra <narendrajagandhane@gmail.com>
2022-03-25 17:24:46 -0700

    For Loop Operation

--- 2022-03-25/src/com/loops/LoopOperationRunner.java
+++ 2022-03-25/src/com/loops/LoopOperationRunner.java
@@ -3,11 +3,11 @@ package com.loops;
 public class LoopOperationRunner {
     public static void main(String[] args) {
         LoopOperation loop1=new LoopOperation(8);
-        System.out.println("\n Number is"+loop1.getNumber());
-        System.out.println("number is a prime "+loop1.isPrime());
-        System.out.println("\nSum upto number is "+loop1.sumUptoN());
-        System.out.println("\n Sum of divisior is"+loop1.sumOfDivisior());
-        System.out.println("\n Pattern Printing Using Loop");
+        System.out.println("\n Number is : "+loop1.getNumber());
+        System.out.println("\n Number is a prime : "+loop1.isPrime());
+        System.out.println("\n Sum upto  "+loop1.getNumber()+" is: "+loop1.sumUptoN());
+        System.out.println("\n Sum of divisior is: "+loop1.sumOfDivisior());
+        System.out.println("\n Pattern Printing Using Loop: ");
         loop1.printTriangle();
 
     }
